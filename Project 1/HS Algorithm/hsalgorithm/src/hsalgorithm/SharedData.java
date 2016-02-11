3efedbc5c99c85c12fc5d8cf632e67205f054d2d
Vibin Daniel <vxd141730@utdallas.edu>
2016-02-10 21:12:04 -0600

    HS 6
    adding shared

--- Project 1/HS Algorithm/hsalgorithm/src/hsalgorithm/SharedData.java
+++ Project 1/HS Algorithm/hsalgorithm/src/hsalgorithm/SharedData.java
@@ -10,83 +10,85 @@ import java.util.Map;
 //import java.util.concurrent.ConcurrentHashMap;
 
 public class SharedData {
-	public static Map<String,Message> buffer=new HashMap<String, Message>();
+	public static Map<String, Message> buffer = new HashMap<String, Message>();
 	public static boolean[] status;
-	public static boolean runPocess=true;
-	public static int roundNo=0;
-	public static final String OUTPUT_FILE="C:\\Workspace\\Github\\DistributedComputing\\Project 1\\HS Algorithm\\hsalgorithm\\src\\hsalgorithm\\output.dat";
-	
-	public static BufferedWriter bufferWriter;
-	public static File outputFile;
-	public static FileWriter fw;
-	public static PrintWriter pw;
-	
-	public static synchronized void sendMessage(int round,int source,int destination,Message message){
-		String key=round+"|"+source+"|"+destination;
-		//System.out.println(key);
-		/*if(message!=null){
-			System.out.println(key+" "+message.getUID()+" "+message.getHops()+" "+message.isForwardMessage());
-		}*/
-		
+	public static boolean runPocess = true;
+	public static int roundNo = 0;
+	public static final String OUTPUT_FILE = "output.dat";
+	public static Boolean defaultAppend = true;
+
+	public static synchronized void sendMessage(int round, int source, int destination, Message message) {
+		String key = round + "|" + source + "|" + destination;
+		// System.out.println(key);
+		/*
+		 * if(message!=null){ System.out.println(key+" "+message.getUID()+" "
+		 * +message.getHops()+" "+message.isForwardMessage()); }
+		 */
+
 		buffer.put(key, message);
 	}
-	
-	public static synchronized Message receiveMessage(int round,int source,int destination){
-		//System.out.println("Map Size :"+buffer.size());
-		String key=round+"|"+source+"|"+destination;
-		//System.out.println("Receiving Message for :"+key);
-		if(buffer.containsKey(key)){
-			Message r=buffer.get(key);
+
+	public static synchronized Message receiveMessage(int round, int source, int destination) {
+		// System.out.println("Map Size :"+buffer.size());
+		String key = round + "|" + source + "|" + destination;
+		// System.out.println("Receiving Message for :"+key);
+		if (buffer.containsKey(key)) {
+			Message r = buffer.get(key);
 			buffer.remove(key);
 			return r;
 		}
 		buffer.remove(key);
 		return null;
 	}
-	
-	public static synchronized void removeMessage(int round,int source,int destination){
-		String key=round+"|"+source+"|"+destination;
+
+	public static synchronized void removeMessage(int round, int source, int destination) {
+		String key = round + "|" + source + "|" + destination;
 		buffer.remove(key);
 	}
-	
-	public static boolean isRoundComplate(){
-		for(boolean b:status){
-			if(b== false){
-				return false;						
+
+	public static boolean isRoundComplate() {
+		for (boolean b : status) {
+			if (b == false) {
+				return false;
 			}
 		}
 		return true;
 	}
-	
-	public static void startWriter(String OutputFile){
-		try{
-			File outputFile = new File(OutputFile);
-			outputFile.createNewFile();
-			
-			fw = new FileWriter(outputFile,true);
-			bufferWriter = new BufferedWriter(fw);
-			pw = new PrintWriter(outputFile);
-		}catch(IOException ex){
+
+	public static void startWriter(String OUTPUT_FILE) {
+		try {
+			File outputFile = new File(OUTPUT_FILE);
+			if (!outputFile.exists())
+				outputFile.createNewFile();
+			defaultAppend = false;
+			writeLine("");
+			defaultAppend = true;
+
+		} catch (IOException ex) {
 			ex.printStackTrace();
 		}
 	}
-	
-	public static synchronized void writeLine(String str){
-		System.out.println(str);
-		try{
-			//File outputFile = new File(OUTPUT_FILE);
+
+	public static synchronized void writeLine(String str) {
+
+		try {
+			FileWriter fw = new FileWriter(new File(OUTPUT_FILE), defaultAppend);
+			BufferedWriter bufferWriter = new BufferedWriter(fw);
+			PrintWriter pw = new PrintWriter(bufferWriter);
+			// File outputFile = new File(OUTPUT_FILE);
+			System.out.println(str);
+			// FileWriter fw = new FileWriter(OUTPUT_FILE,true);
+			// BufferedWriter bufferWriter = new BufferedWriter(fw);
+
+			// bufferWriter.append(str);
+			// bufferWriter.newLine();
+			bufferWriter.write(str);
+			bufferWriter.newLine();
 			
-			//FileWriter fw = new FileWriter(OUTPUT_FILE,true);
-			//BufferedWriter bufferWriter = new BufferedWriter(fw);
-						
-			//bufferWriter.append(str);
-			//bufferWriter.newLine();
-			pw.println(str);
-			
-			//bufferWriter.close();
-		}catch(Exception ex){
+			bufferWriter.close();
+		} catch (Exception ex) {
 			ex.printStackTrace();
 		}
-		
+
 	}
 }
